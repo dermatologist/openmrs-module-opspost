89e4b7fdbb8fc3a8386562bb3be373c961db1c47
Bell Eapen <github_public@gulfdoctor.net>
2020-01-26 18:54:37 -0500

    feat: settings

---
+++ api/src/main/java/org/openmrs/module/opspost/OpspostConstants.java
@@ -0,0 +1,17 @@
+package org.openmrs.module.opspost;
+
+public class OpspostConstants {
+	
+	public static final String OPSPOST_USERNAME = "opspost.username";
+	
+	public static final String OPSPOST_USERNAME_DEFAULT = "admin";
+	
+	public static final String OPSPOST_PASSWORD = "opspost.password";
+	
+	public static final String OPSPOST_PASSWORD_DEFAULT = "Admin123";
+	
+	public static final String OPSPOST_BASEURL = "opspost.baseurl";
+	
+	public static final String OPSPOST_BASEURL_DEFAULT = "";
+	
+}
--- omod/src/main/java/org/openmrs/module/opspost/web/controller/OpspostResourceController.java
+++ omod/src/main/java/org/openmrs/module/opspost/web/controller/OpspostResourceController.java
@@ -1,55 +1,31 @@
 package org.openmrs.module.opspost.web.controller;
 
-import com.google.gson.Gson;
-import org.apache.http.auth.AuthenticationException;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.openmrs.ConceptComplex;
-import org.openmrs.Obs;
-import org.openmrs.Person;
 import org.openmrs.api.AdministrationService;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.opspost.OpspostConstants;
 import org.openmrs.module.opspost.api.OpspostService;
-import org.openmrs.module.webservices.rest.SimpleObject;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.api.RestService;
-import org.openmrs.module.webservices.rest.web.response.ResponseException;
 import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseUriSetup;
 import org.openmrs.module.webservices.rest.web.v1_0.controller.MainResourceController;
-import org.openmrs.obs.ComplexData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Base64;
-import java.util.Date;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
 @Controller
@@ -83,19 +59,29 @@ public class OpspostResourceController extends MainResourceController {
 	
 		String result = "";
 
-		if(opspostService.getItemByApikey(apikey) != null){
-			HttpGet request = new HttpGet("http://tomcat.nuchange.ca:6091/openmrs/ws/rest/v1/session");
+		AdministrationService administrationService = Context.getAdministrationService();
 
-			String encoding = Base64.getEncoder().encodeToString(("admin:Admin123").getBytes());
+		String opsUser = administrationService.getGlobalProperty(OpspostConstants.OPSPOST_USERNAME,
+		    OpspostConstants.OPSPOST_USERNAME_DEFAULT);
+		
+		String opsPw = administrationService.getGlobalProperty(OpspostConstants.OPSPOST_PASSWORD,
+		    OpspostConstants.OPSPOST_PASSWORD_DEFAULT);
+		
+		String opsBaseUrl = administrationService.getGlobalProperty(OpspostConstants.OPSPOST_BASEURL,
+			OpspostConstants.OPSPOST_BASEURL_DEFAULT);
+			
+		String base64UsernamePw = opsUser + ":" + opsPw;
+
+		String serviceUrl = opsBaseUrl + "/openmrs/ws/rest/v1/session";
+
+		System.out.println(serviceUrl);
+		if(opspostService.getItemByApikey(apikey) != null){
+			HttpGet request = new HttpGet(serviceUrl);
+
+			String encoding = Base64.getEncoder().encodeToString((base64UsernamePw).getBytes());
 
 			request.addHeader(HttpHeaders.AUTHORIZATION, "Basic " + encoding);
-			// CredentialsProvider provider = new BasicCredentialsProvider();
-			// provider.setCredentials(
-			// 		AuthScope.ANY,
-			// 		new UsernamePasswordCredentials("admin", "Admin123")
-			// );
-
-			// https://mkyong.com/java/apache-httpclient-examples/
+				// https://mkyong.com/java/apache-httpclient-examples/
 			try (CloseableHttpClient httpClient = HttpClients.createDefault();
 			CloseableHttpResponse response = httpClient.execute(request)) {
 
@@ -109,27 +95,11 @@ public class OpspostResourceController extends MainResourceController {
 				if (entity != null) {
 					// return it as a String
 					result = EntityUtils.toString(entity);
-					System.out.println(result);
+					log.debug(result);
 				}
 
 			}
 
-			// try (CloseableHttpClient httpClient = HttpClientBuilder.create()
-			// 		.setDefaultCredentialsProvider(provider)
-			// 		.build();
-			// 	CloseableHttpResponse response = httpClient.execute(request)) {
-
-			// 	// 401 if wrong user/password
-			// 	System.out.println(response.getStatusLine().getStatusCode());   
-
-			// 	HttpEntity entity = response.getEntity();
-			// 	if (entity != null) {
-			// 		// return it as a String
-			// 		result = EntityUtils.toString(entity);
-			// 		System.out.println(result);
-			// 	}
-
-			// }
 		}
 		return result;
 	}
